81acb660bb3a61761b70c1522d8107f6bf541e1f
lcodrutlucian@gmail.com <lcodrutlucian@gmail.com@ff73f277-aa11-9f8d-8089-d9b01681ab38>
2010-10-16 15:02:21 +0000

    xText projects for eAlf

---
+++ ro.ubbcluj.cs.ealf/src/ro/ubbcluj/cs/ealf/validation/EAlfJavaValidator.java
@@ -0,0 +1,12 @@
+package ro.ubbcluj.cs.ealf.validation;
+
+public class EAlfJavaValidator extends AbstractEAlfJavaValidator {
+
+	// @Check
+	// public void checkGreetingStartsWithCapital(Greeting greeting) {
+	// if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+	// warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
+	// }
+	// }
+
+}
